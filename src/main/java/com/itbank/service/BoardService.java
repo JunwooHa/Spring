a47d87e0199de9ba5e774cb521faa534ae173d8e
JunwooHa <ahffk969j@naver.com>
2023-11-24 13:04:49 +0900

    Logger clear

--- src/main/java/com/itbank/service/BoardService.java
+++ src/main/java/com/itbank/service/BoardService.java
@@ -2,6 +2,8 @@ package com.itbank.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +13,13 @@ import com.itbank.model.dto.BoardDTO;
 @Service
 public class BoardService {
 	
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
 	@Autowired private BoardDAO dao;
 
 	public List<BoardDTO> getBoards() {		
+		log.debug("매서드 호출됨~");
+		
 		return dao.selectAll();
 	}
 
