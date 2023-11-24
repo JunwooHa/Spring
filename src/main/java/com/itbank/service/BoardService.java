447b5d6c8bd3551344b0b4dae8747cd988e2af7a
JunwooHa <44633312+JunwooHa@users.noreply.github.com>
2023-11-24 13:49:13 +0900

    Mybatis 최종 PR (#8)
    * base setting clear
    
    * protect pull request
    
    * 변경2
    
    * db 구성 완료
    
    * board 테이블 출력
    
    * style 적용
    
    * mybatis setting clear
    
    * apply .gitIgnore
    
    * Junit clear
    
    * Logger clear
    
    * Update .gitignore
    
    * LogAOP clear
    
    * ignore_test
    
    * test
    
    * ignore 적용 성공

--- src/main/java/com/itbank/service/BoardService.java
+++ src/main/java/com/itbank/service/BoardService.java
@@ -2,6 +2,8 @@ package com.itbank.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ public class BoardService {
 	@Autowired private BoardDAO dao;
 
 	public List<BoardDTO> getBoards() {		
+		
 		return dao.selectAll();
 	}
 
